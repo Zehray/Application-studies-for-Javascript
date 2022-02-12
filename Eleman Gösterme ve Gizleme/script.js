1cb067d1fac10098f481bb82315095764b1b89bf
Zehray <zehrayatki@gmail.com>
2022-02-12 20:10:12 +0300

    used toggle

--- Eleman Gösterme ve Gizleme/script.js
+++ Eleman Gösterme ve Gizleme/script.js
@@ -6,13 +6,27 @@ let div1 = document.getElementsByClassName("div1")[0];
 let div2 = document.getElementsByClassName("div2")[0]; 
 
 
-button1.addEventListener("click", goster);
-function goster() {
-    div1.style.display="block";
+button1.addEventListener("click", show);
+function show() {
+    // div1.style.display="block";
+    div1.classList.add("show");
+    div1.classList.remove("hidden");
+    div2.classList.add("show");
+    div2.classList.remove("hidden");
 }
 
-button2.addEventListener("click", gizle);
-function gizle() {
-    div1.style.display="none";
+button2.addEventListener("click", hidden);
+function hidden() {
+   // div1.style.display="none";
+   div1.classList.add("hidden");
+   div1.classList.remove("show");
+   div2.classList.add("hidden");
+   div2.classList.remove("show");
+}
+
+button3.addEventListener("click", transition);
+function transition() {
+    div1.classList.toggle("transition");
+    div2.classList.toggle("transition");
 }
 
