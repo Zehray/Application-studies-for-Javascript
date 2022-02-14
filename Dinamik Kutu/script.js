cc1c5fee17d68beae7c5c45bde095f4ff9b0444f
Zehray <zehrayatki@gmail.com>
2022-02-14 17:52:26 +0300

    selection operations of elements

---
+++ Dinamik Kutu/script.js
@@ -0,0 +1,17 @@
+
+/*Elementlerin Seçim İşlemleri */
+var width = document.getElementById("box-width");
+var height = document.getElementById("box-height");
+var bgColor = document.getElementById("background-color");
+var borderWeight = document.getElementById("border-weight");
+var borderType = document.getElementById("border-type");
+var borderColor = document.getElementById("border-color");
+var boxMargin = document.getElementById("box-margin");
+var boxPadding = document.getElementById("box-padding");
+var rotate = document.getElementById("rotate");
+
+var boxCreate = document.getElementById("create");
+
+var rightSide = document.getElementsByClassName("right");
+
+/* Event to determine the function to run when clicked */
