fde943d145840cf45e6b78b4eac534f41f7376f5
Zehray <zehrayatki@gmail.com>
2022-02-25 19:44:35 +0300

    using push function

--- Ekle Çıkar Uygulaması/script.js
+++ Ekle Çıkar Uygulaması/script.js
@@ -1,6 +1,21 @@
-const parcalar = ["Anakart", "Ram", "Ekran Kartı", 1,3,5];
+const parcalar = ["Anakart", "Ram", "Ekran Kartı"];
 //console.log(parcalar);
-parcalar.forEach((eleman, index)=>{
+
+var degisim =document.getElementById("degisim");
+var ekle = document.getElementById("ekle");
+var ekle1 = document.getElementById("ekle1");
+var ekle_btn = document.getElementById("ekle_btn");
+var ekle_btn1 = document.getElementById("ekle_btn1");
+var kaldir = document.getElementById("kaldir");
+var kaldir1 = document.getElementById("kaldir1");
+
+parcalar.forEach((eleman,index)=>{
 //console.log(eleman);
 //console.log(index);
+//console.log(eleman  + " " index); her ikisini yazdırabiliyoruz.
+degisim.innerHTML+= `<div class="alert alert-danger" role="alert">${index}.index: ${eleman}</div>`
 });
+ekle_btn.addEventListener("click", function(){
+    //parcalar.push("Zehra");
+    //console.log(parcalar);
+})
