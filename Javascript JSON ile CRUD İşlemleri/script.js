b5779866eae8bdbe59bf1e828df2cecbc650fb62
Zehray <zehrayatki@gmail.com>
2022-03-06 20:40:19 +0300

    generating data with Javascript POST

--- Javascript JSON ile CRUD İşlemleri/script.js
+++ Javascript JSON ile CRUD İşlemleri/script.js
@@ -1,5 +1,7 @@
+const table = document.getElementById("userTable");
+
 function getUserList(){
-    const table = document.getElementById("userTable");
+
     fetch("https://reqres.in/api/users")
     .then(response=>response.json())
     .then(data=>{
@@ -26,3 +28,46 @@ function getUserList(){
 }
 
 getUserList();
+
+//Tabloyu Yenile
+
+function refreshData(){
+    getUserList();
+}
+
+//Kullanıcı oluştur
+
+function createUser(){
+    let data = {
+        first_name:document.getElementById("first_name").value || "Değer girilmedi",
+        last_name:document.getElementById("last_name").value || "Değer girilmedi",
+        email:document.getElementById("email").value || "Değer girilmedi",
+    };
+    fetch("https://reqres.in/api/users",{
+        method:"POST",
+        headers:{
+           'Content-Type':'application/json'
+},
+         body:JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            table.innerHTML+= `
+            <tr>
+            <td>
+            <input type="text" class="form-control" id="" value="${data.first_name}" />
+            </td>
+            <td>
+            <input type="text" class="form-control" id="" value="${data.last_name}" />
+            </td>
+            <td>
+            <input type="text" class="form-control" id="" value="${data.email}" />
+            </td>
+            <td>
+              <button class="btn btn-warning" onclick="updateUser(${data.id})">Güncelle</button>
+              <button class="btn btn-danger" onclick="deleteUser(${data.id})">Sil</button>
+            </td>
+          </tr>`
+        })
+}
